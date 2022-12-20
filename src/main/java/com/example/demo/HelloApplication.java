3c46c96b62863582a93883d9516caedfee86bca0
zahra kazemi <zahra.k199002@gmail.com>
2022-12-20 14:19:52 +0330

    change to javaFX

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,19 +1,34 @@
-import java.io.File;
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
+import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-public class Main {
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
         FileManagement fileManagement = new FileManagement("C:\\Users\\ALFA\\Documents\\New folder");
         for (int i = 0; i < fileManagement.size; i++) {
             System.out.println(fileManagement.files.get(i));
         }
+        launch();
     }
 }
-
 class FileManagement {
     public int size = 0;
 
