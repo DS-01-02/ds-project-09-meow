76048b600851330060eadd052c5eed3c1d045c2b
ParnianAlasvandian <parnian.alasvandian@gmail.com>
2022-12-25 00:37:09 +0330

    writing panel for phase_1

---
+++ src/main/java/com/example/demo/Home1.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Home1 {
+
+    @FXML
+    private Button btn_add;
+
+    @FXML
+    private Button btn_home;
+
+    @FXML
+    private Button btn_sortdate;
+
+    @FXML
+    private Button btn_sortformat;
+
+    @FXML
+    private Label lbl1;
+
+    @FXML
+    private Label lbl2;
+
+    @FXML
+    private Rectangle rectangle;
+
+    @FXML
+    void btn_delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_add(MouseEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(HelloApplication.class.getResource("AddFile.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(parent);
+        stage.setResizable(false);    //وقتی این فرم ایجاد شده کسی اجازه بزرگتر یا کوچکتر کردن اون رو نداشته باشه
+        stage.setTitle("File Manger");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void click_delete(MouseEvent event) {
+
+    }
+
+    @FXML
+    void click_home(MouseEvent event) {
+
+    }
+
+    @FXML
+    void click_sortdate(MouseEvent event) {
+
+    }
+
+    @FXML
+    void click_sortformat(MouseEvent event) {
+
+    }
+
+}
