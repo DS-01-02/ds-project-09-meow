a8b531910fc2e3e2293726cae8be13a8b6d537f7
ParnianAlasvandian <parnian.alasvandian@gmail.com>
2022-12-25 16:20:07 +0330

    writing sort by date method in phase_1

---
+++ src/main/java/com/example/demo/SortByDate.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.shape.Rectangle;
+
+public class SortByDate {
+
+    @FXML
+    private Button btn_back;
+
+    @FXML
+    private ListView<?> listview;
+
+    @FXML
+    private Rectangle rectangle;
+
+    @FXML
+    private ScrollBar scrollbar;
+
+}
