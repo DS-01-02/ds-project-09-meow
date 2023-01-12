7a06b04ceae9e2565e01df84bd97dbced40950c3
Mahshadghoreyshi <mhshdghoreyshi@gmail.com>
2023-01-12 19:56:50 +0330

    changing some operations in file management for tree

--- src/main/java/com/example/demo/FileManagement.java
+++ src/main/java/com/example/demo/FileManagement.java
@@ -9,34 +9,35 @@ import java.util.ArrayList;
 public class
 
 
-
 FileManagement {
     public int size = 0;
     ArrayList<MyFile1> files = new ArrayList<>();
 
     public FileManagement(String dir) {
-        moveAllFile(dir);
+        moveAllFile(dir,null);
     }
 
-    void moveAllFile(String dir) {
+    void moveAllFile(String dir, Tree<MyFile1> parent) {
         File file = new File(dir);
         File[] tempArr = file.listFiles();
+        Tree<MyFile1> temp = null;
         for (int i = 0; i < tempArr.length; i++) {
             if (tempArr[i].isFile()) {
-                MyFile1 myFile = new MyFile1(tempArr[i].getName(),tempArr[i].getParent(),tempArr[i].getPath());
+                MyFile1 myFile = new MyFile1(tempArr[i].getName(), tempArr[i].getParent(), tempArr[i].getPath());
                 files.add(myFile);
+                
                 try {
                     if (!tempArr[i].getParent().equals(Unzip.dir))
-                        Files.move(Paths.get(tempArr[i].getPath()), Paths.get(Unzip.dir+"\\"+tempArr[i].getName()));
+                        Files.move(Paths.get(tempArr[i].getPath()), Paths.get(Unzip.dir + "\\" + tempArr[i].getName()));
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
                 size++;
             } else {
-                moveAllFile(tempArr[i].getPath());
+                moveAllFile(tempArr[i].getPath(),temp);
             }
         }
-        for (int i = 0; i < tempArr.length; i++){
+        for (int i = 0; i < tempArr.length; i++) {
             if (!tempArr[i].isFile()) {
                 tempArr[i].delete();
             }
